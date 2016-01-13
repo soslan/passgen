e3a2e65199c3d0db9576a25dc039f66e094171b6
Soslan Khubulov <soslanx@gmail.com>
2016-01-13 13:25:15 +0300

    Make length optional. Set up defaults.

--- src/passgen.py
+++ src/passgen.py
@@ -3,7 +3,7 @@ import random
 import argparse
 
 
-def passgen(length=8):
+def passgen(length=12):
     """Generate a strong password with *length* characters"""
     pool = string.ascii_uppercase + string.ascii_lowercase + string.digits
     return ''.join(random.SystemRandom().choice(pool) for _ in range(length))
@@ -11,12 +11,12 @@ def passgen(length=8):
 
 def main():
     parser = argparse.ArgumentParser("Generate strong random password.")
-    parser.add_argument("length",
+    parser.add_argument("-l", "--length",
                         help="the number of characters to generate",
-                        type=int)
+                        type=int, default=12)
     parser.add_argument("-n", "--number",
                         help="how many passwords to generate",
-                        type=int)
+                        type=int, default=10)
 
     args = parser.parse_args()
     for _ in range(args.number):
