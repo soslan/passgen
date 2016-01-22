2ab1fe4346cc46708fd5e4e7dc8cdda740f6f832
Soslan Khubulov <soslanx@gmail.com>
2016-01-22 11:30:43 +0300

    Allow only upper case or only lower case

--- src/passgen.py
+++ src/passgen.py
@@ -3,11 +3,17 @@ import random
 import argparse
 
 
-def passgen(length=12, punctuation=False, digits=True, letters=True):
+def passgen(length=12, punctuation=False, digits=True, letters=True,
+            case="both"):
     """Generate a strong password with *length* characters"""
     pool = []
     if letters:
-        pool.append(string.ascii_uppercase + string.ascii_lowercase)
+        if case == "both":
+            pool.append(string.ascii_uppercase + string.ascii_lowercase)
+        elif case == "upper":
+            pool.append(string.ascii_uppercase)
+        elif case == "lower":
+            pool.append(string.ascii_lowercase)
     if digits:
         pool.append(string.digits)
     if punctuation:
@@ -40,10 +46,27 @@ def main():
     parser.add_argument("--no-letters",
                         help="don't include letters",
                         action='store_false', dest='letters')
+    parser.add_argument("--upper",
+                        help="use only upper case letters",
+                        action='store_true')
+    parser.add_argument("--lower",
+                        help="use only lower case letters",
+                        action='store_true')
     args = parser.parse_args()
     print(args)
+
+    if args.lower and args.upper:
+        # --lower and --upper should not be combined
+        print("Error. --lower and --upper can not be combined.")
+        return 0
+    if args.lower:
+        case = "lower"
+    elif args.upper:
+        case = "upper"
+    else:
+        case = "both"
 
     for _ in range(args.number):
         print(passgen(args.length, punctuation=args.punctuation,
                       digits=args.digits,
-                      letters=args.letters))
+                      letters=args.letters, case=case))
